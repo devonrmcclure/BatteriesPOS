533a780d3d336b03df612f076b8c9e673ae67d44
Devon McClure <devon.r.mcclure@gmail.com>
2016-04-25 15:37:29 -0700

    Fixed typo

--- database/migrations/2016_03_21_142731_create_repair_orders_table.php
+++ database/migrations/2016_03_21_142731_create_repair_orders_table.php
@@ -15,7 +15,7 @@ class CreateRepairOrdersTable extends Migration
         Schema::create('repair_orders', function (Blueprint $table) {
                         $table->increments('id');
                         $table->integer('repair_order_number')->unique();
-                        $table->string('refered_by');
+                        $table->string('referred_by');
                         $table->integer('customer_id')->unsigned();
                         $table->integer('call_if_over')->nullable();
                         $table->boolean('original_receipt')->nullable()->default(NULL);
