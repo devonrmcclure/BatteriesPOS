6afca7c88da85940b813a2d8bad835b62c284251
Devon McClure <devon.r.mcclure@gmail.com>
2016-07-29 13:00:18 -0700

    Allow ability to change quantity, discount, and price

--- resources/assets/js/components/Sales/NewSaleForm.vue
+++ resources/assets/js/components/Sales/NewSaleForm.vue
@@ -7,7 +7,7 @@
         Sold By: <input type="text" name="sale-rep" value="{{rep.first_name}}" readonly/><br />
         
         Date: {{ date | moment }} <br />
-        Total: <input type="text" name="sale-total" value="{{total}}" readonly/><br />
+        Total: $<input type="text" name="sale-total" value="{{total}}" readonly/><br />
         Invoice: <input type="text" name="sale-invoice" value="{{invoice}}" readonly/><br />
         <input class="sku" id="sku" v-model="sku" @change="addProduct" placeholder="Sku" tab-index="1">  <input v-model="quantity" @change="calculatePrice()" placeholder="Quantity">
         <table class="products">
@@ -27,15 +27,15 @@
 
             <tr v-for="product in products">
                 <td @click="removeProduct(product, $index)">X</td>
-                <td><input type="text" name="sku[]" value="{{ product.sku }}"/></td>
+                <td><input type="text" name="sku[]" v-model="product.sku"/></td>
                 <td><input type="text" name="description[]" value="{{ product.description }}" readonly/></td>
-                <td><input type="text" name="quantity[]" value="{{ prices[$index].quantity }}" @change="calculatePrice()"/></td> <!-- This needs to be completely reworked and use something like onChange=getPrices($index)-->
-                <td><input type="text" name="discount[]" v-model="prices[$index].discount" @change="calculateDiscount($index)"/></td>
-                <td><input type="text" name="unit-price[]" value="{{ product.unit_price }}" readonly/></td>
-                <td><input type="text" name="extended[]" value="{{ prices[$index].extended }}" readonly/></td>
-                <td><input type="text" name="pst[]" value="{{ prices[$index].pst }}" readonly/></td>
-                <td><input type="text" name="gst[]" value="{{ prices[$index].gst }}" readonly/></td>
-                <td><input type="text" name="sku-total[]" value="{{ prices[$index].sku_total }}" readonly/></td>
+                <td><input type="text" name="quantity[]" v-model="prices[$index].quantity" @change="updatePrice($index)"/></td> <!-- This needs to be completely reworked and use something like onChange=getPrices($index)-->
+                <td><input type="text" name="discount[]" v-model="prices[$index].discount" @change="updatePrice($index)"/></td>
+                <td><input type="text" name="unit-price[]" v-model="product.unit_price" @change="updatePrice($index)"/></td>
+                <td><input type="text" name="extended[]" v-model="prices[$index].extended" readonly/></td>
+                <td><input type="text" name="pst[]" v-model="prices[$index].pst" readonly/></td>
+                <td><input type="text" name="gst[]" v-model="prices[$index].gst" readonly/></td>
+                <td><input type="text" name="sku-total[]" v-model="prices[$index].sku_total" readonly/></td>
             </tr>
 
         </table>
@@ -134,7 +134,7 @@
 
             },
 
-            calculatePrice(index) {
+            calculatePrice() {
                 // Calc price if item just added
                 // Update price based on index and discount/manually set unit_price
                 // 
@@ -142,41 +142,19 @@
                     this.sku = '';
                     this.quantity = '';
                 }
-
-                if(!index) {
-                    var index = this.products.length - 1;
-                }
+                
+                var index = this.products.length - 1;
 
                 var extended = (this.quantity * this.products[index]['unit_price']).toFixed(2);
 
-                if(this.products[index]['discount'] != null) {
-                    extended = extended * (this.products[index]['discount']/100);
-                }
-
-                if(discount != null && index != null) {
-                    alert('DISCOUNT');
-                    //extended = (this.quantity * this.products[index]['unit_price']).toFixed(2);
-                    //extended = extended * (discount/100);
-                    alert(extended);
-                }
-
-                var pst = 0, gst = 0;
-                //Check if it's taxable.
-                if(this.products[index]['pst'] == 1)
-                {
-                    pst = (extended*0.07).toFixed(2);
-                }
-                if(this.products[index]['gst'] == 1)
-                {
-                    gst = (extended*0.05).toFixed(2);
-                }
-
+                var pst = this.calculatePST(this.products[index], extended);
+                var gst = this.calculateGST(this.products[index], extended);
 
                 var total = Number(Number(extended)+Number(pst)+Number(gst)).toFixed(2);
                 var price = {
                     quantity: this.quantity,
                     extended: Number(extended).toFixed(2),
-                    discount: 0.00,
+                    discount: Number(0.00).toFixed(2),
                     pst: Number(pst).toFixed(2),
                     gst: Number(gst).toFixed(2),
                     sku_total: Number(total).toFixed(2),
@@ -191,12 +169,52 @@
                 }, 0);
             },
 
-            calculateDiscount(index) {
-                console.log(index);
-                var discountPercent = this.prices[index].discount;
+            calculatePST(product, extended) {
+                var pst = 0.00;
+                if(product.pst == 1) {
+                    pst = (extended*0.07).toFixed(2);
+                }
 
-                this.calculatePrice(index);
-                //alert(this.prices[index].discount);
+                return pst;
+            },
+
+            calculateGST(product, extended) {
+                var gst = 0.00;
+                if(product.gst == 1) {
+                    gst = (extended*0.05).toFixed(2);
+                }
+
+                return gst;
+            },
+
+            updatePrice(index) {
+
+                var extended = (this.prices[index].quantity * this.products[index]['unit_price']).toFixed(2);
+
+                if(this.prices[index].discount != 0) {
+                    extended = (extended - (extended * (this.prices[index].discount/100))).toFixed(2);
+                }
+
+                var pst = this.calculatePST(this.products[index], extended);
+                var gst = this.calculateGST(this.products[index], extended);
+
+                var total = Number(Number(extended)+Number(pst)+Number(gst)).toFixed(2);
+                var price = {
+                    quantity: this.prices[index].quantity,
+                    extended: Number(extended).toFixed(2),
+                    discount: Number(this.prices[index].discount).toFixed(2),
+                    pst: Number(pst).toFixed(2),
+                    gst: Number(gst).toFixed(2),
+                    sku_total: Number(total).toFixed(2),
+                };  
+
+                this.$set('prices[index]', price);
+
+                this.sku = '';
+                this.quantity = '';
+                setTimeout(function(){
+                    $("#sku").focus();
+                }, 0);
             },
 
             removeProduct(product, index) {
