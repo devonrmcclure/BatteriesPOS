7641854783e507e38feed3eebe8eceb928aa4302
Devon McClure <devon.r.mcclure@gmail.com>
2016-09-17 17:41:04 -0700

    Design changes

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -5,11 +5,12 @@
 	<h1>Sales <small>{{$date}}</small></h1>
 	<div class="col-md-6 module-container">
 		<div class="module">
+			<button id="show-modal" @click="showRepLoginModal = true">New Sale</button>
 			<daily-sales-stats title="Daily Sales Stats" :location="{{ Auth::User() }}"></daily-sales-stats>
 			<br />
 			<div>
 				<rep-login-modal :show.sync="showRepLoginModal" title="Rep Login" :location="{{ Auth::User() }}"></rep-login-modal>
-				<button id="show-modal" @click="showRepLoginModal = true">New Sale</button>
+				
 			</div>
 	</div>
 </div>
