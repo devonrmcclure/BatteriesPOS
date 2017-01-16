803d80dd745082805e36ff871789965300804b36
Devon McClure <devon.r.mcclure@gmail.com>
2017-01-16 13:02:16 -0800

    re-added invtentory controller due to accidentally removing it

---
+++ app/Http/Controllers/InventoryController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Helpers\Phone;
+
+use App\Http\Requests;
+
+use App\Customer;
+use Auth;
+
+
+class InventoryController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    public function index() {
+    return view('inventory.index');
+    }
+}
