2ff5493a409b85d6fbb752793f2c4deeff2b0d93
Devon McClure <devon.r.mcclure@gmail.com>
2017-01-17 15:24:15 -0800

    meow

--- resources/views/admin/inventory/index.blade.php
+++ resources/views/admin/inventory/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="content">
     <h1>Admin Dashboard <small>{{$date}}</small></h1>
-    <div class="col-md-6 module-container">
+    <div class="col-md-10 module-container">
         <div class="module">
             <admin-inventory :location="{{ Auth::User() }}"></admin-inventory>
             
