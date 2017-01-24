00db8328fc5112e38eed45462dc8a3d33fecca94
Devon McClure <devon.r.mcclure@gmail.com>
2017-01-24 14:47:31 -0800

    added more to close out

--- resources/assets/js/components/CloseOut/CloseOut.vue
+++ resources/assets/js/components/CloseOut/CloseOut.vue
@@ -1,8 +1,7 @@
 <template>
-    {{totalCashSales[0].preTax}}
+    {{totalSales.toFixed(2)}}
 
-    <button type="button" @click="updateStats()">gfd</button>
-    grteghrtesg
+    <button type="button" @click="updateStats()">Calculate</button>
 </template>
 
 
@@ -14,6 +13,10 @@ export default Vue.extend({
     data() {
         return {
             totalCashSales: [],
+            totalInteracSales: [],
+            totalVisaSales: [],
+            totalMasterCardSales: [],
+            totalSales: 0
         }
     },
 
@@ -22,7 +25,10 @@ export default Vue.extend({
     props: ['location'],
 
     ready() {
-        this.getTotalCashSales()
+        this.getTotalCashSales();
+        this.getTotalInteracSales();
+        this.getTotalMasterCardSales();
+        this.getTotalVisaSales();
     },
 
     computed: {
@@ -49,15 +55,82 @@ export default Vue.extend({
                         'preTax': (response.data.data[i].total - (response.data.data[i].total_pst + response.data.data[i].total_gst))
                     };
                         this.totalCashSales.push(data);
+                        this.totalSales += response.data.data[i].total;
                 }
-
-                console.log('whyyyyyyyyyyyyy');
-                    
             }, function(response) {
                 //Error
                 if(response.status === 404)
                 {
-                    this.totalCashSales = 5;        
+                    this.totalCashSales = [];        
+                }
+            });
+        },
+
+        getTotalInteracSales() {
+            var url = '/api/v0/invoice?created_at=' + this.today + '&location=' + this.location.name + '&limit=all&payment_method=Interac';
+            this.$http.get(url, {api_token: this.location.api_token})
+            .then(function(response) {
+                //Success
+                for(var i = 0; i < response.data.data.length; i++) {
+                    
+                    var data = {
+                        'withTax': response.data.data[i].total,
+                        'preTax': (response.data.data[i].total - (response.data.data[i].total_pst + response.data.data[i].total_gst))
+                    };
+                        this.totalInteracSales.push(data);
+                        this.totalSales += response.data.data[i].total;
+                }                    
+            }, function(response) {
+                //Error
+                if(response.status === 404)
+                {
+                    this.totalInteracSales = [];        
+                }
+            });
+        },
+
+        getTotalVisaSales() {
+            var url = '/api/v0/invoice?created_at=' + this.today + '&location=' + this.location.name + '&limit=all&payment_method=Visa';
+            this.$http.get(url, {api_token: this.location.api_token})
+            .then(function(response) {
+                //Success
+                for(var i = 0; i < response.data.data.length; i++) {
+                    
+                    var data = {
+                        'withTax': response.data.data[i].total,
+                        'preTax': (response.data.data[i].total - (response.data.data[i].total_pst + response.data.data[i].total_gst))
+                    };
+                        this.totalVisaSales.push(data);
+                        this.totalSales += response.data.data[i].total;
+                }                    
+            }, function(response) {
+                //Error
+                if(response.status === 404)
+                {
+                    this.totalVisaSales = [];        
+                }
+            });
+        },
+
+        getTotalMasterCardSales() {
+            var url = '/api/v0/invoice?created_at=' + this.today + '&location=' + this.location.name + '&limit=all&payment_method=Mastercard';
+            this.$http.get(url, {api_token: this.location.api_token})
+            .then(function(response) {
+                //Success
+                for(var i = 0; i < response.data.data.length; i++) {
+                    
+                    var data = {
+                        'withTax': response.data.data[i].total,
+                        'preTax': (response.data.data[i].total - (response.data.data[i].total_pst + response.data.data[i].total_gst))
+                    };
+                        this.totalMasterCardSales.push(data);
+                        this.totalSales += response.data.data[i].total;
+                }                    
+            }, function(response) {
+                //Error
+                if(response.status === 404)
+                {
+                    this.totalMasterCardSales = [];        
                 }
             });
         },
@@ -65,10 +138,17 @@ export default Vue.extend({
         updateStats() {
             this.clear();
             this.getTotalCashSales();
+            this.getTotalInteracSales();
+            this.getTotalMasterCardSales();
+            this.getTotalVisaSales();
         },
 
         clear() {
             this.totalCashSales = [];
+            this.totalInteracSales = [];
+            this.totalMasterCardSales = [];
+            this.totalVisaSales = [];
+            this.totalSales = 0;
         }
     }
 });
