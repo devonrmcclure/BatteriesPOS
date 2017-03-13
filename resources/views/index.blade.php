26b1567def07f148784f30d0d8bde9390ea076d7
Devon McClure <devon.r.mcclure@gmail.com>
2017-03-13 14:52:46 -0700

    Made new sale modal popup on any page when clicking bottom link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="content">
-	<h1>Dashboard <small>{{$date}}</small></h1>
+	<h1>Todo <small>{{$date}}</small></h1>
 
 	<div class="col-md-6 module-container">
 		<div class="module" id="app">
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -3,9 +3,9 @@
 		<ul>
 			<!-- Define Sidebar-nav Links -->
 			@section('sidebar-nav-links')
-				{{-- <li class="ripple-effect"><a href="/"><i class="material-icons md-24 indigo500">home</i> Dashboard</a></li>
-				<li class="ripple-effect"><a href="/customers"><i class="material-icons md-24 green500">person</i> Customers</a></li> --}}
-				<li class="ripple-effect"><a href="/sales"><i class="material-icons md-24 green500">shopping_cart</i> New Sale</a></li>
+				<li class="ripple-effect"><a href="/"><i class="material-icons md-24 indigo500">home</i> Dashboard</a></li>
+				{{-- <li class="ripple-effect"><a href="/customers"><i class="material-icons md-24 green500">person</i> Customers</a></li> --}}
+				<li class="ripple-effect"><a href="#" id="show-modal" @click="showRepLoginModal = true"><i class="material-icons md-24 green500">shopping_cart</i> New Sale</a></li>
 				<li class="ripple-effect"><a href="/repair-orders"><i class="material-icons md-24 grey700">build</i> Repair Orders</a></li>
 				<li class="ripple-effect"><a href="/part-orders"><i class="material-icons md-24 deep-purple500">local_shipping</i> Part Orders</a></li>
 				<li class="ripple-effect"><a href="/inventory"><i class="material-icons md-24 redA700">content_paste</i> Inventory</a></li>
@@ -14,4 +14,9 @@
 			<!-- End Sidebar-nav links -->
 		</ul>
 	</div>
+</div>
+
+<div>
+	<rep-login-modal :show.sync="showRepLoginModal" title="Rep Login" :location="{{ Auth::User() }}"></rep-login-modal>
+	
 </div>
