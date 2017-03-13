203f1dec89702e80202cb62bbaea9dc079f22609
Devon McClure <devon.r.mcclure@gmail.com>
2017-03-13 13:20:18 -0700

    fix issue with close out and move nav to bottom

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -3,9 +3,9 @@
 		<ul>
 			<!-- Define Sidebar-nav Links -->
 			@section('sidebar-nav-links')
-				<li class="ripple-effect"><a href="/"><i class="material-icons md-24 indigo500">home</i> Dashboard</a></li>
-				<li class="ripple-effect"><a href="/customers"><i class="material-icons md-24 green500">person</i> Customers</a></li>
-				<li class="ripple-effect"><a href="/sales"><i class="material-icons md-24 green500">shopping_cart</i> Sales</a></li>
+				{{-- <li class="ripple-effect"><a href="/"><i class="material-icons md-24 indigo500">home</i> Dashboard</a></li>
+				<li class="ripple-effect"><a href="/customers"><i class="material-icons md-24 green500">person</i> Customers</a></li> --}}
+				<li class="ripple-effect"><a href="/sales"><i class="material-icons md-24 green500">shopping_cart</i> New Sale</a></li>
 				<li class="ripple-effect"><a href="/repair-orders"><i class="material-icons md-24 grey700">build</i> Repair Orders</a></li>
 				<li class="ripple-effect"><a href="/part-orders"><i class="material-icons md-24 deep-purple500">local_shipping</i> Part Orders</a></li>
 				<li class="ripple-effect"><a href="/inventory"><i class="material-icons md-24 redA700">content_paste</i> Inventory</a></li>
