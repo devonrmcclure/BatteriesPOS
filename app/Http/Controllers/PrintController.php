51cdb52e24a7c4e81103ad4629aea62e3bc2e78f
Devon McClure <devon.r.mcclure@gmail.com>
2017-03-26 18:33:25 -0700

    finished sales invoice printing styles and stuff

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -10,12 +10,19 @@ use App\Http\Requests;
 
 class PrintController extends Controller
 {
+
+    public function __construct()
+    {
+        // TODO: Also check if user is admin.
+        $this->middleware('auth');
+    }
+
     public function sales_invoice($id) {
         // Get the invoice and corresponding sales data based on ID. 
         // Render it to a view in /print/sales/index.blade.php
 
         $invoice = Invoice::where('id', $id)->with('sale', 'customer')->first();
-        dd($invoice->toArray());
+        //dd($invoice->toArray());
 
         return view('print/sales/index')->with('invoice', $invoice);
     }
--- resources/views/print/sales/index.blade.php
+++ resources/views/print/sales/index.blade.php
@@ -21,10 +21,64 @@
     @show
 </head>
 <body>
-    hi.fdsfsd
-
     <!-- This will be variables and stuff from what is receive via getting an invoice -->
-    {{$invoice->sale[0]->sku}}
+
+    <div class="sales-invoice">
+        <div class="customer-info">
+            <p class="name">{{$invoice->customer->first_name}} {{$invoice->customer->last_name}}</p>
+            <p class="address">{{$invoice->customer->address}}, {{$invoice->customer->city}}, {{$invoice->customer->province}}</p>
+            <p class="primary_phone">{{$invoice->customer->primary_phone}}</p>
+        </div>
+
+        <div class="invoice-info">
+            <p class="invoice-number"><span class="heading">Invoice: </span>{{$invoice->id}}</p>
+            <p class="method"><span class="heading">Method: </span>{{$invoice->payment_method}}</p>
+            <p class="staff"><span class="heading">Sold By: </span>{{$invoice->staff}}</p>
+            <p cladd="date"><span class="heading">Date: </span>{{$invoice->created_at->format('M j, Y @ g:i A')}}</p>
+        </div>
+
+        <div class="invoice-products">
+            <table>
+                <tr>
+                    <th>Sku</th>
+                    <th>Description</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>Extended</th>
+                    <th>PST</th>
+                    <th>GST</th>
+                    <th>Total</th>
+                </tr>
+
+                @foreach($invoice->sale as $product)
+                    <tr> 
+                        <td><p class="product-sku">{{$product->sku}}</p></td>
+                        <td><p class="product-description">{{$product->description}}</p></td>
+                        <td><p class="product-quantity">{{$product->quantity}}</p></td>
+                        <td><p class="product-price">${{number_format($product->price, 2)}}</p></td>
+                        <td><p class="product-extended">${{number_format($product->extended, 2)}}</p></td>
+                        <td><p class="product-pst>">${{number_format($product->pst, 2)}}</p></td>
+                        <td><p class="product-gst">${{number_format($product->gst, 2)}}</p></td>
+                        <td><p class="product-total">${{number_format($product->total, 2)}}</p></td>
+                    </tr>
+                @endforeach
+
+                <tr>
+                    <td colspan=4></td>
+                    <th>Totals</th>
+                    <th>${{number_format($invoice->total_pst, 2)}}</th>
+                    <th>${{number_format($invoice->total_gst, 2)}}</th>
+                    <th>${{number_format($invoice->total, 2)}}</th>
+
+                </tr>
+            </table>
+        </div>
+
+        <div class="invoice-comment">
+            <p class="comment">{{$invoice->invoice_comment}}</p>
+        </div>
+
+    </div>
 
     <script>
         //window.print();
