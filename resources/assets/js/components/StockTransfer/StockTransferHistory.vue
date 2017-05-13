6af47bab7d2b8e725875930429d462a2b7679c1a
Devon McClure <devon.r.mcclure@gmail.com>
2017-05-12 17:12:31 -0700

    more style for stock page

--- resources/assets/js/components/StockTransfer/StockTransferHistory.vue
+++ resources/assets/js/components/StockTransfer/StockTransferHistory.vue
@@ -14,12 +14,19 @@
                     <p class="order-number">Order WR01HO</p>
                     <p class="order-date">2017-05-06</p>
                     <p class="view-more"><a href="#"><i class="material-icons md-24 md-black"> launch</i></a></p>
-                    <p class="order-status"><i class="material-icons md-24 yellowA700">warning </i></p>
+                    <p class="order-status"><i class="material-icons md-24 red500">warning </i></p>
                 </div>
 
-                <div class="order">
+                <div class="order completed">
                     <p class="order-number">Order WR00HO</p>
                     <p class="order-date">2017-05-01</p>
+                    <p class="view-more"><a href="#"><i class="material-icons md-24 md-black"> launch</i></a></p>
+                    <p class="order-status"><i class="material-icons md-24 green700">check_circle </i></p>
+                </div>
+
+                <div class="order completed">
+                    <p class="order-number">Order WR00HO</p>
+                    <p class="order-date">2017-01-01</p>
                     <p class="view-more"><a href="#"><i class="material-icons md-24 md-black"> launch</i></a></p>
                     <p class="order-status"><i class="material-icons md-24 green700">check_circle </i></p>
                 </div>
@@ -30,7 +37,7 @@
                     <p class="order-number">Order RM05WR</p>
                     <p class="order-date">2017-05-06</p>
                     <p class="view-more"><a href="#"><i class="material-icons md-24 md-black"> launch</i></a></p>
-                    <p class="order-status"><i class="material-icons md-24 yellowA700">warning </i></p>
+                    <p class="order-status"><i class="material-icons md-24 red500">warning </i></p>
                 </div>
             </div>
         </div>
